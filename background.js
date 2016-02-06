a84f8d994fdf1b09debd88659588f40bf9048cc0
Tema <egoroof@users.noreply.github.com>
2016-02-06 23:40:50 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.downloads.download({
     url: 'https://avatars0.githubusercontent.com/u/2605831', // any file here
-    filename: 'changed filename.png',
-    saveAs: false // this should override Chrome setting
+    filename: 'changed filename.png' // this propery is ignored
 });
